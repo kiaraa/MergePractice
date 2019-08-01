d4a9945fe771f48599669302b5f8d28d4581dd65
Kiara Atkins <atkinsk98@gmail.com>
2019-08-01 12:04:31 -0400

    messgae

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		System.out.println("My name is Kiara");
+		System.out.println("My name is Kiara -- merging is coolish");
 		System.out.println("My name is Atiba");
 	}
 
